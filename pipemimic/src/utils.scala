df49c1e8b2c3c40800c50592e5c1ec0147d27e8a
YikeZhou <u201714805@hust.edu.cn>
2021-03-19 15:35:47 +0800

    utils.scala: add Last function in ListUtils & add DotGraph functions DotTest.scala: unit test for Dot

--- pipemimic/src/utils.scala
+++ pipemimic/src/utils.scala
@@ -1,6 +1,7 @@
 package pipemimic
 
 import Stages.{Pipeline, GlobalEvent, GlobalGraph}
+import scala.collection.immutable.HashMap
 
 object ListUtils {
 
@@ -12,6 +13,14 @@ object ListUtils {
 
   def Head[T](default: T, l: List[T]): T = {
     if (l.isEmpty) default else l.head
+  }
+
+  def Last[T](l: List[T], default: T): T = {
+    l match {
+      case Nil => default
+      case List(t) => t
+      case head :: next => Last(next, default)
+    }
   }
 
   def Tail[T](x: List[T]): List[T] = {
@@ -208,5 +217,174 @@ object GlobalGraphIDUtils {
     }
     
     helper(p, n, 0, 0)
+  }
+}
+
+object Dot {
+  private val startColor = "#d6fced"
+  private val middleColor = "#8dfbe4"
+  private val finishColor = "#83e0d8"
+
+  val edgeColor = HashMap(
+    "ProgramOrder" -> "#ff0000", // Blue
+    "IntraLocation" -> "#00cc00", // Green
+    "IntraEvent" -> "#000000", // Black
+    "RF" -> "#ff0000", // Bright red
+    "WS" -> "#ff0000", // Med red
+    "FR" -> "#ff0000", // Dark red
+    "Other" -> "#0000ff" // Yellow
+  )
+
+  def EdgeColor(l1: Int, l2: Int, edgeString: String): String = {
+    val color = if (l1 == 0 && l2 == 0) edgeColor("ProgramOrder") else edgeColor.getOrElse(edgeString, "Other")
+    s"""color="$color""""
+  }
+
+  def EdgeDotted(n1: Int, n2: Int, lThick: List[(Int, Int)]): String = {
+    lThick match {
+      case Nil => ""
+      case _ => if (lThick.contains((n1, n2))) "" else "style=dashed"
+    }
+  }
+
+  def FormatString(l: List[String]): String = {
+    l.filter(!_.isEmpty) match {
+      case Nil => ""
+      case x => x.mkString("[", ",", "]")
+    }
+  }
+
+  def natPairToString(fString: Int => String, fStageEvent: Int => (Int, Int), lThick: List[(Int, Int)], np: (Int, Int, String)): String = {
+    val (n1, n2) = (np._1, np._2)
+    val (l1, e1) = fStageEvent(n1)
+    val (l2, e2) = fStageEvent(n2)
+    val formatString = FormatString(
+      EdgeDotted(n1, n2, lThick) ::
+      EdgeColor(l1, l2, np._3) ::
+      { if (lThick.contains((n1, n2))) "penwidth=5" else "" } ::
+      { if (e1 == e2) "" else "constraint=false" } :: Nil
+    )
+    s"  ${fString(n1)} -> ${fString(n2)}$formatString; // ($n1, $n2) ${np._3}\n"
+  }
+
+  def GroupNodesByEvent(f: Int => (Int, Int), l: List[Int]): List[List[Int]] = {
+    l match {
+      case head :: next => {
+        val (n, e) = f(head)
+        ListUtils.AppendUniqueToNth(GroupNodesByEvent(f, next), e, head)
+      }
+      case Nil => Nil
+    }
+  }
+
+  def EdgeListToNodeList(l: List[(Int, Int, String)], f: Int => (Int, Int), rNodes: List[Int]): List[Int] = {
+    l match {
+      case head :: next => {
+        val (h1, h2) = (head._1, head._2)
+        val _rNodes = ListUtils.AddUnique(ListUtils.AddUnique(rNodes, h1), h2)
+        EdgeListToNodeList(next, f, _rNodes)
+      }
+      case Nil => rNodes
+    }
+  }
+
+  def SubgraphClusterEntry(nodeToString: Int => String, lBold: List[Int], nodeNumber: Int): String = {
+    val format = {
+      if (lBold.isEmpty) ""
+      else if (nodeNumber == ListUtils.Head(0, lBold)) s""" [style=filled,color="$startColor"]"""
+      else if (nodeNumber == ListUtils.Last(lBold, 0)) s""" [style=filled,color="$finishColor"]"""
+      else if (lBold.contains(nodeNumber)) s""" [style=filled,color="$middleColor"]"""
+      else ""
+    }
+    s"    ${nodeToString(nodeNumber)}$format; // $nodeNumber\n"
+  }
+
+  def SubgraphCluster(eventNumber: Int, nodeToString: Int => String, lBold: List[Int], listOfNodesForEvent: List[Int]): String = {
+    s"""  subgraph cluster_$eventNumber  {
+        style=filled;
+        color=white;
+        label="Event$eventNumber"
+        ${listOfNodesForEvent.map(SubgraphClusterEntry(nodeToString, lBold, _)).mkString}  }
+    """
+  }
+
+  def SubgraphClusters(nodeToString: Int => String, lBold: List[Int], listOfNodesForEachEvent: List[List[Int]]): String = {
+    def helper(eventNumber: Int, nodeToString: Int => String, lBold: List[Int], listOfNodesForEachEvent: List[List[Int]]): String = {
+      listOfNodesForEachEvent match {
+        case head :: next => SubgraphCluster(eventNumber, nodeToString, lBold, head) ++ helper(eventNumber + 1, nodeToString, lBold, next)
+        case Nil => ""
+      }
+    }
+    
+    helper(0, nodeToString, lBold, listOfNodesForEachEvent)
+  }
+
+  def rankNodes(f: Int => Int, pMax: Int, l: List[Int]): (List[List[Int]], List[Int]) = {
+    def helper(f: Int => Int, pMax: Int, l: List[Int], r1: List[List[Int]], r2: List[Int]): (List[List[Int]], List[Int]) = {
+      l match {
+        case head :: next => if (pMax <= f(head)) helper(f, pMax, next, r1, ListUtils.AddUnique(r2, head)) else helper(f, pMax, next, ListUtils.AppendToNth(r1, f(head), head), r2)
+        case Nil => (r1, r2)
+      }
+    }
+    
+    helper(f, pMax, l, Nil, Nil)
+  }
+
+  def standaloneNode(f: Int => String, lBold: List[Int], n: Int): String = {
+    val format = {
+      if (lBold.isEmpty) ""
+      else if (n == ListUtils.Head(0, lBold)) s""" [style=filled,color="$startColor"]"""
+      else if (n == ListUtils.Last(lBold, 0)) s""" [style=filled,color="$finishColor"]"""
+      else if (lBold.contains(n)) s""" [style=filled,color="$middleColor"]"""
+      else ""
+    }
+    s"    ${f(n)}$format; // node $n\n"
+  }
+
+  def rank(fString: Int => String, lBold: List[Int], l: List[Int]): String = {
+    s"  {rank=same;\n${l.map(standaloneNode(fString, lBold, _)).mkString}  }\n"
+  }
+
+  def ranks(fString: Int => String, lBold: List[Int], l: List[List[Int]]): String = {
+    l.map(rank(fString, lBold, _)).mkString
+  }
+
+  def unranked(fString: Int => String, lBold: List[Int], l: List[Int]): String = {
+    l.map(standaloneNode(fString, lBold, _)).mkString
+  }
+
+  def EdgeStrings(fString: Int => String, fStageEvent: Int => (Int, Int), l: List[(Int, Int, String)], lThick: List[(Int, Int)]): String = {
+    l.map(natPairToString(fString, fStageEvent, lThick, _)).mkString
+  }
+
+  /**
+    * Build a dot graph from the description of the graph, plus some formatting options.
+    *
+    * @param name the title of the graph
+    * @param lNormal the set of edges in the graph
+    * @param fStageEvent given a vertex, return the location and event the vertex represents
+    * @param fString given a vertex, return a label for the vertex
+    * @param lBold a list of nodes that should be bolded/filled in
+    * @param lThick a list of edges that should be colored and thickened
+    * @param pMax the length of the pipeline. Vertices with locations > pMax (i.e., cache events) will noe necessarily be clustered with the pipeline.
+    * @return graph described in dot grammar
+    */
+  def DotGraph(name: String, lNormal: List[(Int, Int, String)], fStageEvent: Int => (Int, Int), fString: Int => String, lBold: List[Int], lThick: List[(Int, Int)], pMax: Int): String = {
+    val ms = new TinyTimer("dot_graph")
+    ms.reset
+    val fStage: Int => Int = fStageEvent(_)._1
+    val fEvent: Int => Int = fStageEvent(_)._2
+    val lNodes = EdgeListToNodeList(lNormal, fStageEvent, Nil)
+    val lSameEvent = GroupNodesByEvent(fStageEvent, lNodes)
+    val (lRanked, lUnranked) = rankNodes(fStage, pMax, lNodes)
+    val result = s"""// nodes per column: $pMax
+    digraph PipeCheck {
+      labellloc="t";
+      label="$name";
+      newrank=true;
+      ${SubgraphClusters(fString, lBold, lSameEvent)}${ranks(fString, lBold, lRanked)}${unranked(fString, lBold, lUnranked)}${EdgeStrings(fString, fStageEvent, lNormal, lThick)}}
+      """
+    println(ms)
+    result
   }
 }
