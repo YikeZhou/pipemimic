d83de6cc6e30435b2510c19f357b45358c96c8f4
YikeZhou <u201714805@hust.edu.cn>
2021-03-22 16:52:25 +0800

    tree.scala: add scala doc and improve code style GraphTreeTest.scala: add more test cases

--- pipemimic/test/src/GraphTreeTest.scala
+++ pipemimic/test/src/GraphTreeTest.scala
@@ -6,10 +6,48 @@ import org.scalatest.flatspec.AnyFlatSpec
 import GraphTree._
 
 class GraphTreeTest extends AnyFlatSpec {
-  behavior of "GraphTree"
+  "GraphTree" should "calculate DNFOfTree correctly" in {
+    val gt1 = GraphTreeAnd(List(
+      GraphTreeLeaf("A", List((1, 2, "a"))),
+      GraphTreeLeaf("B", List((3, 4, "b")))
+    ))
+    val gt2 = GraphTreeAnd(List(
+      GraphTreeLeaf("A", List((1, 2, "a"))),
+      GraphTreeOr(List(
+        GraphTreeLeaf("B", List((3, 4, "b"))),
+        GraphTreeLeaf("C", List((5, 6, "c")))
+      ))
+    ))
+    val gt3 = GraphTreeAnd(List(
+      GraphTreeLeaf("A", List((1, 2, "a"))),
+      GraphTreeLeaf("B", List((7, 8, "d"))),
+      GraphTreeOr(List(
+        GraphTreeLeaf("C", List((3, 4, "b"))),
+        GraphTreeLeaf("D", List((5, 6, "c")))
+      ))
+    ))
+    val gt4 = GraphTreeAnd(List(
+      GraphTreeLeaf("A", List((1, 2, "a"))),
+      GraphTreeAnd(List(
+        GraphTreeEmptyLeaf[Int],
+        GraphTreeLeaf("B", List((3, 4, "b")))
+      ))
+    ))
+    val gt5 = GraphTreeAnd(List(
+      GraphTreeLeaf("A", List((1, 2, "a"))),
+      GraphTreeOr(List.empty[GraphTree[Int]])
+    ))
 
-  it should "calculate DNFOfTree correctly" in {
-    val gt1 = GraphTreeAnd(List(GraphTreeLeaf("A", List((1, 2, "a"))), GraphTreeLeaf("B", List((3, 4, "b")))))
+    val trees = Seq(gt1, gt2, gt3, gt4, gt5)
+
+    def print_node(i: Int): String = i.toString
+
     assert(DNFOfTree(gt1) == List(("AB", List((1, 2, "a"), (3, 4, "b")))))
+    assert(DNFOfTree(gt2) == List(("AB", List((1, 2, "a"), (3, 4, "b"))), ("AC", List((1, 2, "a"), (5, 6, "c")))))
+    assert(DNFOfTree(gt3) == List(("ABC", List((1, 2, "a"), (7, 8, "d"), (3, 4, "b"))), ("ABD", List((1, 2, "a"), (7, 8, "d"), (5, 6, "c")))))
+    assert(DNFOfTree(gt4) == List(("AB", List((1, 2, "a"), (3, 4, "b")))))
+    assert(DNFOfTree(gt5) == Nil)
+
+    for (i <- 0 to 4) println(DNFStringOfTree(print_node, trees(i)))
   }
 }
