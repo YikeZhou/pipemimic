6170197ea98dd65779b02a922f71d0b00c61e546
YikeZhou <u201714805@hust.edu.cn>
2021-04-11 16:22:05 +0800

    All local reordering and special edge map needed for test are prepared

--- pipemimic/src/pipemimic/pipeline/package.scala
+++ pipemimic/src/pipemimic/pipeline/package.scala
@@ -1,10 +1,8 @@
 package pipemimic
 
-import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
 package object pipeline {
-  // TODO add useful definitions such as FIFO, NoSpecialEdges, etc.
 
   /* Local Reordering */
 
@@ -19,9 +17,15 @@ package object pipeline {
   val NoOrderGuarantees: LocalReordering = _ => _ => Nil
 
   /**
-    * The output order is guaranteed to match some previous ordering
+    * The output order is guaranteed to match some previous ordering.
+    * e.g. reorder buffer
     */
-  def Restore(n: Int): LocalReordering = e => _ => e.nthDefault(n, Nil)
+  def restore(localStageIndex: Int): LocalReordering = e => _ => e(localStageIndex)
+
+  /** Only operations to the same address are guaranteed to maintain their ordering. */
+  def sameAddressOrdered: LocalReordering = _ => predecessor => predecessor filter {
+    case (former, latter) => former.addr == latter.addr
+  }
 
   /* Special Edge Maps */
 
@@ -30,21 +34,29 @@ package object pipeline {
     */
   val NoSpecialEdges: SpecialEdgeMap = _ => _ => _ => Nil
 
-  @tailrec
-  def StoreBufferSpecialEdges(c: Int, n: Int)(eventBefore: List[Event])(e: Event)(eventAfter: List[Event]): GlobalGraph = {
-    eventAfter match {
-      case h :: t => h.dirn match {
-        case Some(Direction.R) => StoreBufferSpecialEdges(c, n)(eventBefore)(e)(t)
-        case Some(Direction.W) => List(((6 * n, e.eiid), (5 + 6 * c, h.eiid), "StoreBuffer"))
-        case _ => StoreBufferSpecialEdges(c, n)(eventBefore)(e)(t)
+  /** This store buffer only allows one outstanding unacknowledged store at a time. */
+  def storeBufferSpecialEdges(srcPerformStage: Location, dstPerformStage: Location)
+                             (eventBefore: List[Event])(e: Event)(eventAfter: List[Event]): GlobalGraph = {
+    eventAfter foreach { after =>
+      if (after.dirn.contains(Direction.W)) {
+        /* current store must write to memory before next store value enter store buffer */
+        return List(((srcPerformStage, e.eiid), (dstPerformStage, after.eiid), "StoreBuffer"))
       }
-      case Nil => Nil
     }
+    Nil
   }
 
-  def FenceTSOSpecialEdges(c: Int, n: Int)(eventBefore: List[Event])(e: Event)(eventAfter: List[Event]): GlobalGraph = {
+  /** Fence instruction will go through five stage in risc pipeline */
+  def fenceTSOSpecialEdges(storePerformStage: Location, loadPerformStage: Location)
+                          (eventBefore: List[Event])(e: Event)(eventAfter: List[Event]): GlobalGraph = {
+    // TODO check implementation in reality
     require(e.action.isInstanceOf[MemoryFence])
-    val edges = ListBuffer.empty[((Location, Eiid), (Location, Eiid), String)]
+    require(eventBefore.forall(!_.action.isInstanceOf[MemoryFence]) &&
+      eventAfter.forall(!_.action.isInstanceOf[MemoryFence]))
+
+    val edges = ListBuffer.empty[(GlobalEvent, GlobalEvent, String)]
+
+    /* events before must finish before events after start */
     eventBefore foreach {
       case Event(eiid, Iiid(proc, _), action) => action match {
 
@@ -52,19 +64,58 @@ package object pipeline {
           /* happens before all events in eventsAfter */
           for (latter <- eventAfter) {
             if (latter.dirn.contains(Direction.W))
-              edges += (((6 * proc + 3, eiid), (6 * n, latter.eiid), "FenceTSO"))
+              edges += (((loadPerformStage, eiid), (storePerformStage, latter.eiid), "FenceTSO"))
             else if (latter.dirn.contains(Direction.R))
-              edges += (((6 * proc + 3, eiid), (6 * proc + 3, latter.eiid), "FenceTSO"))
+              edges += (((loadPerformStage, eiid), (loadPerformStage, latter.eiid), "FenceTSO"))
           }
 
         case Access(Direction.W, _, _) =>
           /* happens before all store events in eventsAfter */
           for (latter <- eventAfter if latter.dirn.contains(Direction.W))
-            edges += (((6 * n, eiid), (6 * n, latter.eiid), "FenceTSO"))
+            edges += (((storePerformStage, eiid), (storePerformStage, latter.eiid), "FenceTSO"))
 
         case _ =>
       }
     }
+
     edges.toList
   }
+
+  /** This is a intra-event edge from read to cache line invalidation */
+  def loadCacheLineSpecialEdges(loadPerformStage: Location, cacheInvPerformStage: Location)
+                               (eventsBefore: List[Event])(e: Event)(eventsAfter: List[Event]): GlobalGraph = {
+    List( ((loadPerformStage, e.eiid), (cacheInvPerformStage, e.eiid), "LoadCacheLine") )
+  }
+
+  /** For every load operation, if following load at same address current load should happen before latter cache inv */
+  def speculativeLoadReorderingSpecialEdges(loadPerformStage: Location, cacheInvPerformStage: Location)
+                                           (eventsBefore: List[Event])(e: Event)(eventsAfter: List[Event]): GlobalGraph = {
+    val edges = ListBuffer.empty[(GlobalEvent, GlobalEvent, String)]
+    eventsAfter foreach {
+      case Event(eiid, _, Access(direction, address, _)) if direction == Direction.R && e.addr.contains(address) =>
+        edges += (((loadPerformStage, e.eiid), (cacheInvPerformStage, eiid), "SpeculativeLoadReordering"))
+    }
+    edges.toList
+  }
+
+  /** special edges for load path option */
+  def loadSpecialEdges(loadPerformStage: Location, cacheInvPerformStage: Location)
+                      (eventsBefore: List[Event])(e: Event)(eventsAfter: List[Event]): GlobalGraph = {
+    loadCacheLineSpecialEdges(loadPerformStage, cacheInvPerformStage)(eventsBefore)(e)(eventsAfter) :::
+      speculativeLoadReorderingSpecialEdges(loadPerformStage, cacheInvPerformStage)(eventsBefore)(e)(eventsAfter)
+  }
+
+  /** When the store executes, squash following load at same address if it already performed. */
+  def storeLoadSpecialEdges(storePerformStage: Location, loadPerformStage: Location)
+                           (eventsBefore: List[Event])(e: Event)(eventsAfter: List[Event]): GlobalGraph = {
+    require(e.isWrite)
+
+    val edges = ListBuffer.empty[(GlobalEvent, GlobalEvent, String)]
+    eventsAfter foreach {
+      case Event(eiid, _, Access(direction, address, _)) if direction == Direction.R && e.addr.contains(address) =>
+        edges += (((storePerformStage, e.eiid), (loadPerformStage, eiid), "StoreLoad"))
+    }
+    edges.toList
+  }
+
 }
