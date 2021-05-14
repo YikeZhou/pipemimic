bbd49101a87f769c950f61de34bcc259aa27626b
YikeZhou <u201714805@hust.edu.cn>
2021-05-14 14:30:46 +0800

    Add split version for litmus timing

--- profiling/litmus-profiling.py
+++ profiling/litmus-profiling.py
@@ -4,15 +4,13 @@ Project: profiling
 File Created: Thursday, 13th May 2021 7:54:18 pm
 Author: zyk
 -----
-Last Modified: Thursday, 13th May 2021 10:59:27 pm
+Last Modified: Friday, 14th May 2021 2:12:34 pm
 Modified By: zyk
 -----
 2021 - HUST
 '''
 
 import csv
-
-f = open('./profiling/litmus-timing.tex', 'w', encoding='utf-8')
 
 legend = list()
 matrix = list()
@@ -29,21 +27,35 @@ with open("./profiling/litmus-profiling.csv", newline='\n') as csvfile:
       # name + time(ms)
       matrix.append(row)
 
-xticklabels = list()
-for arch in range(4):
-  f.write("\\addplot coordinates {\n")
 
-  for (row, idx) in zip(matrix, range(len(matrix))):
-    name = row[0]
-    time_ms = row[arch + 1]
-    
-    # \addplot coordinates { (name,t1) (name t2) ... };
-    f.write("(" + str(idx) + "," + str(time_ms) + ")\n")
-    print(idx, name)
-    xticklabels.append(name)
-  f.write("(" + str(len(matrix)) + ",0)\n")
-  f.write("};\n")
+
+split = 4 # split into 4 files
+assert len(matrix) % split == 0
+segment = int(len(matrix) / split)
+print("segment: " + str(segment))
+
+for i in range(split):
+  filename = './profiling/litmus-timing-' + str(i) + '.tex'
+  f = open(filename, 'w', encoding='utf-8')
+
+  xticklabels = list()
   
-f.write("\\legend{" + ','.join(legend) + "}\n")
-f.write("% xticklabels={" + ','.join(xticklabels) + "},")
-f.close()
+  for arch in range(4):
+    f.write("\\addplot coordinates {\n")
+
+    for (row, idx) in zip(matrix[i * segment:(i + 1) * segment], range(segment)):
+      name = row[0] 
+      time_ms = row[arch + 1]
+      
+      # \addplot coordinates { (name,t1) (name t2) ... };
+      f.write("(" + str(idx) + "," + str(time_ms) + ")\n")
+      # print(idx, name)
+      xticklabels.append(name)
+    
+    f.write("(" + str(segment) + ",0)\n") # redundant column
+    f.write("};\n")
+    
+  if i == 0:
+    f.write("\\legend{" + ','.join(legend) + "}\n") # only show legend on top
+  f.write("% xticklabels={" + ','.join(xticklabels) + "},")
+  f.close()
