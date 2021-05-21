790d7ce5ca4db342547e5a53d830d0c60cdd56ad
YikeZhou <u201714805@hust.edu.cn>
2021-05-21 23:24:19 +0800

    Update test suite for same address

--- pipemimic/test/src/pipemimic/ppo/SameAddressTest.scala
+++ pipemimic/test/src/pipemimic/ppo/SameAddressTest.scala
@@ -1,13 +1,15 @@
 package pipemimic.ppo
 
 import org.scalatest.flatspec.AnyFlatSpec
-import pipemimic.RISCTest
+import pipemimic.pipeline.PipelineFactory
 
 class SameAddressTest extends AnyFlatSpec {
   behavior of "SameAddress"
 
   it should "yield correct dot graph" in {
-    val p = RISCTest.RISCPipeline(1)
+    val f = new PipelineFactory
+    val constructor = f.createPipeline("WR")
+    val p = constructor.pipelineWithCore(1)
     val sa = new SameAddress(p)
     for (po <- ProgramOrder.values) {
       println(sa.isSatisfied(po) + po.toString)
